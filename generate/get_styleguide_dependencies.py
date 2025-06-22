fcf12f79d48def5793f3577339d216833fbf5e27
PJ Reiniger <pj.reiniger@gmail.com>
2025-06-22 01:01:53 -0400

    Regen

--- generate/get_styleguide_dependencies.py
+++ generate/get_styleguide_dependencies.py
@@ -2,7 +2,7 @@ from bazelrio_gentool.deps.dependency_container import DependencyContainer
 
 
 def get_styleguide_dependencies():
-    version = "2024.11.19"
+    version = "2025.06.22"
     year = "1"
     group = DependencyContainer("rules_wpi_styleguide", version, year, "")
 
